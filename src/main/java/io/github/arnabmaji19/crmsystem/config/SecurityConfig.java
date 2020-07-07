96ffecff7123f1466c507dc733dc40d8e84459a8
Arnab Maji <arnabmaji1999@gmail.com>
2020-07-07 20:58:14 +0530

    Add basic security authentication

---
+++ src/main/java/io/github/arnabmaji19/crmsystem/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package io.github.arnabmaji19.crmsystem.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SecurityConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .jdbcAuthentication()
+                .dataSource(dataSource);
+    }
+}
---
+++ src/main/java/io/github/arnabmaji19/crmsystem/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package io.github.arnabmaji19.crmsystem.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
